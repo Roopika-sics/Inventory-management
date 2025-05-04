c6f5077986c17ce139f59da9ba0aa5f02ac0f66b
Vvk_0105 <vivekbabu.sics@gmail.com>
2025-05-04 17:29:15 +0530

    place order done

--- buyer/urls.py
+++ buyer/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('product/<int:pk>/', views.product_detail, name='product-detail'),
     path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.cart_view, name='cart'),
+    path('place_order/' , views.place_order, name='place_order'),
 
 ]
--- buyer/views.py
+++ buyer/views.py
@@ -1,8 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from accounts.models import User
 from .models import Buyer
 from django.contrib import messages
-from products.models import Product, CartItem
+from products.models import Product, CartItem, Order, OrderItem
 from categories.models import Category
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
@@ -42,10 +43,14 @@ def buyer_home(request):
     categories = Category.objects.all()
     return render(request, 'buyer/buyer_home.html', {'smartphones': smartphones, 'categories': categories})
 
+@login_required
+@never_cache
 def product_detail(request, pk):
     product = Product.objects.get(pk=pk)
     return render(request, 'buyer/product_detail.html', {'product': product})
 
+@login_required
+@never_cache
 def add_to_cart(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     quantity = int(request.POST.get('quantity', 1))
@@ -71,7 +76,43 @@ def add_to_cart(request, product_id):
 
     return redirect('product-detail', pk=product_id)
 
+@login_required
+@never_cache
 def cart_view(request):
     cart_items = CartItem.objects.filter(user=request.user)
     total = sum(item.total_price() for item in cart_items)
     return render(request, 'buyer/cart.html', {'cart_items': cart_items, 'total': total})
+
+
+
+@login_required
+def place_order(request):
+    cart_items = CartItem.objects.filter(user=request.user)
+    total = sum(item.total_price() for item in cart_items)
+    if not cart_items.exists():
+        return redirect('cart')
+
+    if request.method == 'POST':
+        address = request.POST.get('address')
+        total = sum(item.product.base_price * item.quantity for item in cart_items)
+        
+        order = Order.objects.create(user=request.user, delivery_address=address, total_price=total)
+        
+        for item in cart_items:
+            OrderItem.objects.create(
+                order=order,
+                product=item.product,
+                quantity=item.quantity,
+                price=item.product.base_price
+            )
+            # Reduce stock
+            item.product.stock -= item.quantity
+            item.product.save()
+        
+        # Clear cart
+        cart_items.delete()
+
+        return render(request, 'buyer/order_success.html', {'order': order})
+
+    total_price = sum(item.product.base_price * item.quantity for item in cart_items)
+    return render(request, 'buyer/place_order.html', {'cart_items': cart_items, 'total_price': total})
